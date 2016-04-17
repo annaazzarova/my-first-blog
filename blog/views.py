855428e33ccde860e2f384caa5292f7a51abcc60
Anna Azarova <anna.azzarova1993@gmail.com>
2016-04-17 13:06:47 +0300

    Add page, where is number of added posts

--- blog/views.py
+++ blog/views.py
@@ -1,52 +1,54 @@
 import urllib
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 import feedparser, time, datetime
+import requests
 
 
 def db_update(request):
-    d = feedparser.parse('http://tnt-dance.ru/load/rss/')
-    for field in d.entries:
-        post = Post()
-        post.title = field.title
-        post.url = field.link
-        post.author = request.user
-        post.text = field.summary
-        created_date = field.published_parsed
-        post.published_date = timezone.now()
-        post.created_date = time.strftime('%Y-%m-%dT%H:%M:%SZ', created_date)
-        post.save()
-    d = feedparser.parse('http://гранитанца.рф/feed/')
-    for field in d['entries']:
-        post = Post()
-        post.title = field.title
-        post.url = field.link
-        post.text = field.summary
-        post.author = request.user
-        created_date = field.published_parsed
-        post.published_date = timezone.now()
-        post.created_date = time.strftime('%Y-%m-%dT%H:%M:%SZ', created_date)
-        post.save()
-    d = feedparser.parse('http://blog.ted.com/feed/')
-    for field in d['entries']:
-        post = Post()
-        post.title = field.title
-        post.url = field.link
-        post.text = field.summary
-        post.author = request.user
-        created_date = field.published_parsed
-        post.published_date = timezone.now()
-        post.created_date = time.strftime('%Y-%m-%dT%H:%M:%SZ', created_date)
-        post.save()
-    return HttpResponse("ok")
+    n = 0;
+    posts = Post.objects.all()
+    urls = ['http://tnt-dance.ru/load/rss/', 'http://гранитанца.рф/feed/', 'http://blog.ted.com/feed/']
+    for url in urls:
+        d = feedparser.parse(url)
+        for field in d.entries:
+            post_exist = False
+            post = Post()
+            post.title = field.title
+            post.url = field.link
+            post.author = request.user
+            post.text = field.summary
+            created_date = field.published_parsed
+            post.published_date = timezone.now()
+            post.created_date = time.strftime('%Y-%m-%dT%H:%M:%SZ', created_date)
+            for p in posts:
+                if p.title == post.title:
+                    post_exist = True
+            if not post_exist:
+                n += 1
+                post.save()
+    print(requests.get('http://tnt-dance.ru/load/rss/').text)
+    return render(request, 'blog/db_upload.html', {'n': n})
 
 
 def post_list(request):
-    posts = Post.objects.order_by('created_date')
+    posts_list = Post.objects.order_by('-created_date')
+    paginator = Paginator(posts_list, 20)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b(tutzt8_80hdhh$*+_+ew4366tnes%iv_2*o_i9k=8b^c5_@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['annaazzarova.pythonanywhere.com']
 
 
 # Application definition
